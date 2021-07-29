49447fab30ed1cf1abec589724704c484d4c4e78
lisswart <arnoudlisa@gmail.com>
2021-07-28 21:12:08 -0400

    redo assignment after fetching upstream

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 require 'sqlite3'
+require 'pry'
 
-require_relative "../lib/student"
+require_relative "../lib/student"  # this line loads the Student class
 
-DB = { conn: SQLite3::Database.new("db/students.db") }
+DB = { conn: SQLite3::Database.new("db/students.db") }  # this line loads the database
